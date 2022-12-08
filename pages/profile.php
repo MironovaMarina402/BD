fdd4ef8cfb2dbd5e10bf0c6f96205b0474da656e
MironovaMarina402 <99708276+MironovaMarina402@users.noreply.github.com>
2022-12-08 09:45:29 +0700

    Update profile.php

--- pages/profile.php
+++ pages/profile.php
@@ -23,15 +23,11 @@
     <div>
         <p><strong>Имя: </strong> <?php echo $employee['first_name'] ?></p>
         <p><strong>Фамилия: </strong> <?php echo $employee['last_name'] ?></p>
-        <p><strong>Дата рождения: </strong> <?php echo $employee['birth_date'] ?></p>
-        <p><strong>Пол: </strong> <?php echo $employee['gender'] ?></p>
         <p><strong>Подразделение:</strong> <?php echo $employee['dept_name'] ?></p>
         <p><strong>Должность:</strong> <?php echo $employee['title'] ?></p>
-        <p><strong>Трудоустроен с:</strong> <?php echo $employee['from_date'] ?></p>
-        <p><strong>Трудоустроен до:</strong> <?php print $employee['to_date'] ?></p>
-        <p><strong>Работает до сих пор?</strong>
-        <strong><?php if ($employee['to_date'] == '9999-01-01') {echo "ДА";} ?></strong>
-        <strong><?php if ($employee['to_date'] !== '9999-01-01') {echo "НЕТ";} ?></strong></p> 
+        <p><strong>Устроен до:</strong> <?php echo $employee['to_date'] ?></p>
+        <p><strong>Работает до сих пор?</strong> <strong><?php if ($employee['to_date'] == '9999-01-01') {echo "ДА";} ?></strong>
+                                                 <strong><?php if ($employee['to_date'] !== '9999-01-01') {echo "НЕТ";} ?></strong></p> 
     </div>
 
 </body>
